2f32fd32920eccd5f9960087aba286745cd29085
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-02 05:29:42 +0100

    importing_in_exploiter

--- src/Contracts/the-rewarder/FlashLoanReceiver.sol
+++ src/Contracts/the-rewarder/FlashLoanReceiver.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.12;
 
+import {RewardToken} from "./RewardToken.sol";
+import {DamnValuableToken} from "../DamnValuableToken.sol";
+import {AccountingToken} from "./AccountingToken.sol";
+
 interface IFlashLoanReceiver {
     function receiveFlashLoan(uint256 amount) external;
 
@@ -18,12 +22,24 @@ interface ITheRewarderPool {
 contract FlashLoanReceiver {
     address private _attacker;
     address private _flashLoanerPool;
+    address private _dvt;
+    address private _rewardToken;
+    address private _accToken;
 
     error IsNotFlashLoanerPool();
 
-    constructor(address attacker, address flashLoanerPool) {
+    constructor(
+        address attacker,
+        address flashLoanerPool,
+        address dvtAddress,
+        address rewardTokenAddress,
+        address accTokenAddress
+    ) {
         _attacker = attacker;
         _flashLoanerPool = flashLoanerPool;
+        _dvt = dvtAddress;
+        _rewardToken = rewardTokenAddress;
+        _accToken = accTokenAddress;
     }
 
     function receiveFlashLoan(uint256 amount) external {
