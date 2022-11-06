462095c438c472f52c23d30ada2c9fdd33b6dc4f
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-07 00:36:05 +0100

    layout

---
+++ src/Contracts/puppet/FlashPuppet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+contract FlashPuppet {
+    address private attacker;
+    address private puppetPool;
+    address private uniswapPool;
+}
