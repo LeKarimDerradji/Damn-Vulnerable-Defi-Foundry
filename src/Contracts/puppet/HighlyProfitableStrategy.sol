8fb6e1e172be9c2eea312f5233a904ae5afb1c9d
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-08 21:15:36 +0100

    interfaces

--- src/Contracts/puppet/HighlyProfitableStrategy.sol
+++ src/Contracts/puppet/HighlyProfitableStrategy.sol
@@ -5,6 +5,27 @@ interface IPuppetPool {
     function borrow(uint256) external;
 }
 
+interface UniswapV1Exchange {
+    function addLiquidity(
+        uint256 min_liquidity,
+        uint256 max_tokens,
+        uint256 deadline
+    ) external payable returns (uint256);
+
+    function balanceOf(address _owner) external view returns (uint256);
+
+    function tokenToEthSwapInput(
+        uint256 tokens_sold,
+        uint256 min_eth,
+        uint256 deadline
+    ) external returns (uint256);
+
+    function getTokenToEthInputPrice(uint256 tokens_sold)
+        external
+        view
+        returns (uint256);
+}
+
 contract HighlyProfitableStrategy {
     address private _attacker;
     address private _puppetPool;
