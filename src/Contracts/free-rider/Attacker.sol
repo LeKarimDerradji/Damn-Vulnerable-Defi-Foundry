9e878336e3c10fdf0324714fbc6d716cb8b3a971
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-16 10:28:00 +0100

    layout_2

--- src/Contracts/free-rider/Attacker.sol
+++ src/Contracts/free-rider/Attacker.sol
@@ -3,14 +3,18 @@ pragma solidity 0.8.12;
 
 import {IERC721Receiver} from "openzeppelin-contracts/token/ERC721/IERC721Receiver.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/security/ReentrancyGuard.sol";
+import {IERC721} from "openzeppelin-contracts/token/ERC721/IERC721.sol";
 
 contract Attacker is IERC721Receiver, ReentrancyGuard {
     address private immutable _buyer;
+    address private immutable _attacker;
+    IERC721 private immutable _nft;
+    uint256 private _received;
 
-    constructor(address buyer_, address _nft) payable {
+    constructor(address buyer_, address nft_) payable {
         _buyer = buyer_;
-        nft = IERC721(_nft);
-        IERC721(_nft).setApprovalForAll(msg.sender, true);
+        _nft = IERC721(nft_);
+        IERC721(nft_).setApprovalForAll(msg.sender, true);
     }
 
     // Read https://eips.ethereum.org/EIPS/eip-721 for more info on this function
@@ -20,15 +24,19 @@ contract Attacker is IERC721Receiver, ReentrancyGuard {
         uint256 _tokenId,
         bytes memory
     ) external override nonReentrant returns (bytes4) {
-        require(msg.sender == address(nft));
+        require(msg.sender == address(_nft));
         // This require might be the way to exploit it.
-        require(tx.origin == partner);
+        require(tx.origin == _attacker);
         require(_tokenId >= 0 && _tokenId <= 5);
-        require(nft.ownerOf(_tokenId) == address(this));
+        require(_nft.ownerOf(_tokenId) == address(this));
 
-        received++;
-        if (received == 6) {
-            payable(partner).sendValue(JOB_PAYOUT);
+        _received++;
+        if (_received == 6) {
+            uint256 i;
+            for(i =0; i < _received; i++) {
+                IERC721(_nft).transfer(address(_buyer), i);
+            }
+            
         }
 
         return IERC721Receiver.onERC721Received.selector;
