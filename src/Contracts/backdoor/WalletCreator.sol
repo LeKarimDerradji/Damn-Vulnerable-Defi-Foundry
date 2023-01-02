0593e8a2a2939a6615506fd2e668c131459ca29a
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-01-03 00:13:06 +0100

    commit

--- src/Contracts/backdoor/WalletCreator.sol
+++ src/Contracts/backdoor/WalletCreator.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.12;
 import {GnosisSafe} from "gnosis/GnosisSafe.sol";
 import {GnosisSafeProxy} from "gnosis/proxies/GnosisSafeProxy.sol";
 import {GnosisSafeProxyFactory} from "gnosis/proxies/GnosisSafeProxyFactory.sol";
+import {WalletRegistry} from "./WalletRegistry.sol";
 
 // Your contract
 contract MyContract {
@@ -12,43 +13,41 @@ contract MyContract {
     // Address of the Gnosis Safe proxy factory contract
     address immutable gnosisSafeProxyFactory;
 
+    WalletRegistry immutable walletRegistry;
+
     constructor(
         address gnosisSafeMasterCopy_,
-        address gnosisSafeProxyFactory_
+        address gnosisSafeProxyFactory_,
+        WalletRegistry walletRegistry_
     ) {
         gnosisSafeMasterCopy = gnosisSafeMasterCopy_;
         gnosisSafeProxyFactory = gnosisSafeProxyFactory_;
+        walletRegistry = walletRegistry_;
     }
 
-    function createGnosisSafeWallet() public {
-        // Generate a unique saltNonce value
+    function createGnosisSafeWallet(
+        address[] memory owners,
+        uint256 threshold
+    ) public {
+        // Encode the data for the GnosisSafe "setup" function call
+        bytes memory initializer = abi.encodeWithSignature(
+            "setup(address[],uint256)",
+            owners,
+            threshold
+        );
+
+        // Create a unique nonce for the proxy contract
         uint256 saltNonce = uint256(
             keccak256(abi.encodePacked(block.timestamp, msg.sender))
         );
-        // Call the createProxy function of the GnosisSafeProxyFactory contract
-        GnosisSafeProxy proxy = GnosisSafeProxyFactory(gnosisSafeProxyFactory)
-            .createProxy(
-                gnosisSafeMasterCopy,
-                abi.encodeWithSelector(
-                    GnosisSafe.setup.selector,
-                    1,
-                    [msg.sender]
-                )
-            );
-    }
-
-     // Encode the data for the GnosisSafe "setup" function call
-        bytes memory initializer = abi.encodeWithSignature("setup(address[],uint256)", owners, threshold);
-
-        // Create a unique nonce for the proxy contract
-        uint256 saltNonce = uint256(keccak256(abi.encodePacked(now, msg.sender)));
 
         // Call the createProxyWithCallback function to create the proxy contract
-        proxyFactory.createProxyWithCallback(
-            address(GnosisSafe), // The singleton contract
-            initializer, // The initializer data
-            saltNonce, // The nonce
-            address(this) // The callback contract
-        );
+        GnosisSafeProxy proxy = GnosisSafeProxyFactory(gnosisSafeProxyFactory)
+            .createProxyWithCallback(
+                address(gnosisSafeMasterCopy), // The singleton contract
+                initializer, // The initializer data
+                saltNonce, // The nonce
+                walletRegistry // The callback contract
+            );
     }
 }
